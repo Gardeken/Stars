e8b689c54bd989167ca0d4d3104da794316859e5
Gardeken <gardeken931@gmail.com>
2024-07-16 17:45:07 -0400

    inicios

---
+++ src/client/components/nav.jsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="logo">stars</h1>
+      <div>
+        <a href="/">Inicio</a>
+        <a href="/about">Sobre nosotros</a>
+        <a href="/contact">Contactanos</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/client/views/home.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Nav from "../components/nav";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Nav></Nav>
+    </Fragment>
+  );
+};
+
+export default Home;
