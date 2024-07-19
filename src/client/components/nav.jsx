5f9c9c2e91d02fe97ad92563da0d91d5302f9833
Gardeken <gardeken931@gmail.com>
2024-07-19 16:26:58 -0400

    creando home

--- src/client/components/nav.jsx
+++ src/client/components/nav.jsx
@@ -2,10 +2,16 @@ const Nav = () => {
   return (
     <nav className="navbar">
       <h1 className="logo">stars</h1>
-      <div>
-        <a href="/">Inicio</a>
-        <a href="/about">Sobre nosotros</a>
-        <a href="/contact">Contactanos</a>
+      <div className="container-links-nav">
+        <a className="link-nav" href="/">
+          Inicio
+        </a>
+        <a className="link-nav" href="/about">
+          Sobre nosotros
+        </a>
+        <a className="link-nav" href="/contact">
+          Contactanos
+        </a>
       </div>
     </nav>
   );
--- src/client/views/home.jsx
+++ src/client/views/home.jsx
@@ -5,6 +5,13 @@ const Home = () => {
   return (
     <Fragment>
       <Nav></Nav>
+      <main className="container-main">
+        <div className="container-gif">
+          <video autoPlay loop muted>
+            <source src="src/client/assets/estrellas.mp4" type="video/mp4" />
+          </video>
+        </div>
+      </main>
     </Fragment>
   );
 };
