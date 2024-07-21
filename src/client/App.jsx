063266d89ac3dbee0dd63c43242383ea9ea9f87b
Gardeken <gardeken931@gmail.com>
2024-07-21 16:09:57 -0400

    avanzando el home

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Home from "./views/home";
 
 function App() {
-  return <Home></Home>
+  return <Home></Home>;
 }
 
 export default App;
--- src/client/views/home.jsx
+++ src/client/views/home.jsx
@@ -6,11 +6,28 @@ const Home = () => {
     <Fragment>
       <Nav></Nav>
       <main className="container-main">
+        <div className="container-texto-vid">
+          <section className="eslogan">
+            <h2>Creamos</h2>
+            <h3>Recuerdos</h3>
+          </section>
+          <div className="container-btn-crea">
+            <button>Crea tu cruadro</button>
+          </div>
+        </div>
         <div className="container-gif">
           <video autoPlay loop muted>
             <source src="src/client/assets/estrellas.mp4" type="video/mp4" />
           </video>
         </div>
+        <img
+          className="constelaciones img1"
+          src="src/client/assets/constelaciones.webp"
+        />
+        <img
+          className="constelaciones img2"
+          src="src/client/assets/constelaciones2.webp"
+        />
       </main>
     </Fragment>
   );
