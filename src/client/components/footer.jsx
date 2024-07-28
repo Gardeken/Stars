cf8ce847eb959326c2e3b285701367f511b1284b
Gardeken <gardeken931@gmail.com>
2024-07-28 18:28:58 -0400

    terminando home

---
+++ src/client/components/footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="footer">Copyright © 2024 Stars </footer>;
+};
+
+export default Footer;
--- src/client/components/nav.jsx
+++ src/client/components/nav.jsx
@@ -1,16 +1,15 @@
 const Nav = () => {
   return (
     <nav className="navbar">
-      <h1 className="logo">stars</h1>
+      <h1 className="logo">
+        <a href="">Stars</a>
+      </h1>
       <div className="container-links-nav">
         <a className="link-nav" href="/">
           Inicio
         </a>
-        <a className="link-nav" href="/about">
+        <a className="link-nav" href="#aboutus">
           Sobre nosotros
-        </a>
-        <a className="link-nav" href="/contact">
-          Contactanos
         </a>
       </div>
     </nav>
