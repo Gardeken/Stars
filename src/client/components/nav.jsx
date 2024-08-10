b4b35c046b214ae10b98c99167e319836f02cea8
Gardeken <gardeken931@gmail.com>
2024-08-10 01:41:20 -0400

    terminando nav

--- src/client/components/nav.jsx
+++ src/client/components/nav.jsx
@@ -1,10 +1,38 @@
 const Nav = () => {
+  function animacionNav() {
+    const links = document.querySelector(".container-links-sec-nav");
+    links.classList.toggle("animacion-click-nav");
+  }
   return (
     <nav className="navbar">
       <h1 className="logo">
         <a href="">Stars</a>
       </h1>
       <div className="container-links-nav">
+        <a className="link-nav" href="/">
+          Inicio
+        </a>
+        <a className="link-nav" href="#aboutus">
+          Sobre nosotros
+        </a>
+      </div>
+      <div onClick={animacionNav} className="svg-ham">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="svg-ham"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </div>
+      <div className="container-links-sec-nav">
         <a className="link-nav" href="/">
           Inicio
         </a>
